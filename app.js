f03b04faf818ee87ff0c0f7dd15c64bb3acf72a0
Molina <brenda.molina@accenture.com>
2018-06-27 16:39:48 -0500

    add new routes

--- app.js
+++ app.js
@@ -3,6 +3,30 @@
 let express = require('express');
 let app = express();
 
-/* Your code */
+app.listen(3000);
+app.use(express.static(__dirname + '/app_client'));
+
+app.post('/api/login',(req,res)=>{
+    console.log("login");
+    res.send("login ;D");
+});
+
+app.post('/api/register', (req,res)=>{
+    console.log("registered");
+    res.send("register");
+});
+app.get('/logout', (req,res)=>{
+    console.log("logout");
+    res.send("logout");
+});
+app.get('/api/profile', (req,res)=>{
+    console.log("profile");
+    res.send("profile");
+});
+
+// app.get('*',(req,res)=>{
+//     res.sendFile(path.join(__dirname + '/app_client/index.html'));
+// })
 
 module.exports = app;
+
